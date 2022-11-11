d71116e7d9837a9efff6a210a8eafd65960369d0
KEKOKASPARY <117411835+KEKOKASPARY@users.noreply.github.com>
2022-11-10 21:32:10 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require 'config.php';
 
 $lista = [];
 $sql = $pdo->query("SELECT * FROM usuario");
-if($sql->rowCount()>0){
+if($sql->rowCount() > 0){
     $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
 }
 ?>
@@ -18,15 +18,16 @@ if($sql->rowCount()>0){
         <th>Ações</th>
     </tr>
     <?php foreach($lista as $usuario): ?>
-        <<tr>
+        <tr>
             <td><?=$usuario['id'];?></td>
             <td><?=$usuario['nome'];?></td>
             <td><?=$usuario['email'];?></td>
             <td>
-                <a href="editar.php?id=<?=$usuario['id];?>"]>[Editar ]</a>
-                <a href="excluir.php">[Excluir ]</a>
+              <a href="editar.php?id=<?=$usuario['id'];?>">[Editar ]</a>
+              <a href="excluir.php?id=<?=$usuario['id'];?>">[Excluir ]</a>
             </td>
         </tr>
      <?php endforeach; ?>   
 </table>
+
 <a href="cadastrar.php">Cadastrar Usuário</a>
